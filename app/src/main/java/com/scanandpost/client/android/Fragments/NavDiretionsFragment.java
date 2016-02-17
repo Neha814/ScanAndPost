e61f514824accc417935bf1479d038c2bbee88fc
nehabh <bhardwajneha3433@gmail.com>
2016-02-17 17:47:51 +0530

    Second commit

--- app/src/main/java/com/scanandpost/client/android/Fragments/NavDiretionsFragment.java
+++ app/src/main/java/com/scanandpost/client/android/Fragments/NavDiretionsFragment.java
@@ -1,14 +1,24 @@
 package com.scanandpost.client.android.Fragments;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.scanandpost.client.android.R;
 import com.scanandpost.client.functions.GPSTracker;
 
@@ -18,14 +28,14 @@ import com.scanandpost.client.functions.GPSTracker;
  */
 public class NavDiretionsFragment extends Fragment {
 
-    /*GoogleMap mGoogleMap;
-    MarkerOptions markerOptions;
+    GoogleMap mGoogleMap;
     SupportMapFragment fragment;
     Double FromLat, FromLng;
     GPSTracker gps;
 
 
     View rootView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.nav_directions, container, false);
@@ -49,13 +59,16 @@ public class NavDiretionsFragment extends Fragment {
             mGoogleMap = fragment.getMap();
         }
 
-        if ( ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
-
-            *//*ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                  LocationService.MY_PERMISSION_ACCESS_COURSE_LOCATION);*//*
-            mGoogleMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
-
         mGoogleMap.setMyLocationEnabled(true);
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
         mGoogleMap.getUiSettings().setCompassEnabled(true);
@@ -72,5 +85,7 @@ public class NavDiretionsFragment extends Fragment {
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-    }*/
+        Log.e("map is added", "map is added");
+
+    }
 }
